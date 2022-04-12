4a1afc833642c69e8f4b0786bff795df62a4cdc1
TeslaLive <103488250+TeslaLive@users.noreply.github.com>
2022-04-12 14:18:34 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+let N = 0
+while (!(input.buttonIsPressed(Button.A))) {
+    if (input.buttonIsPressed(Button.A)) {
+        N += 1
+        if (true) {
+            N = 0
+        }
+        if (N == 3) {
+        	
+        } else {
+        	
+        }
+    }
+}
